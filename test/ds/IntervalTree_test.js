3263707d8fe8a5b279ca1b2c7e56bb4b5330c90f
James Silver <jamsilver@gmail.com>
2016-09-01 16:36:17 +0100

    Add lookups for overlapping intervals

--- test/ds/IntervalTree_test.js
+++ test/ds/IntervalTree_test.js
@@ -28,4 +28,21 @@ describe('DS - IntervalTree', () => {
     intervalTree.insert(0, 10, 'd');
     intervalTree.lookup(8).should.deep.equal(['d']);
   });
+
+  it ('should find overlapping intervals', () => {
+    const intervalTree = new IntervalTree();
+    intervalTree.insert(2, 4, 'a');
+    intervalTree.insert(2, 5, 'b');
+    intervalTree.insert(4, 6, 'c');
+    intervalTree.overlap(0, 1).should.deep.equal([]);
+    intervalTree.overlap(1, 2).should.deep.equal(['a', 'b']);
+    intervalTree.overlap(2, 3).should.deep.equal(['a', 'b']);
+    intervalTree.overlap(3, 4).should.deep.equal(['a', 'b', 'c']);
+    intervalTree.overlap(4, 5).should.deep.equal(['a', 'b', 'c']);
+    intervalTree.overlap(5, 6).should.deep.equal(['b', 'c']);
+    intervalTree.overlap(6, 7).should.deep.equal(['c']);
+    intervalTree.overlap(7, 8).should.deep.equal([]);
+    intervalTree.overlap(2, 6).should.deep.equal(['a', 'b', 'c']);
+    intervalTree.overlap(0, 8).should.deep.equal(['a', 'b', 'c']);
+  })
 });
