c567472abadc06da14ccaee8d931e3cbd475e988
Jonathan Skeate <skeate@gmail.com>
2020-03-12 20:12:10 -0400

    Begin move of BST to TS

--- src/ds/__tests__/BinarySearchTree.spec.ts
+++ src/ds/__tests__/BinarySearchTree.spec.ts
@@ -24,12 +24,6 @@ describe('DS - BinarySearchTree', () => {
     expect(a.size).toEqual(5);
   });
 
-  /** @test {BinarySearchTree#toArray} */
-  it('should convert into a sorted array', () => {
-    const a = new BinarySearchTree([1, 5, 7, 4, 3, 7]);
-    expect(a.toArray()).toEqual([1, 3, 4, 5, 7, 7]);
-  });
-
   /** @test {BinarySearchTree#remove} */
   it('should remove nodes', () => {
     /* This should generate a tree like
@@ -40,33 +34,30 @@ describe('DS - BinarySearchTree', () => {
      * 1   4 6   8
      */
     let a = new BinarySearchTree([5, 3, 7, 1, 4, 6, 8]);
-    expect(a.toArray()).toEqual([1, 3, 4, 5, 6, 7, 8]);
+    expect([...a]).toEqual([1, 3, 4, 5, 6, 7, 8]);
     // no children
-    a.remove(8);
-    expect(a.toArray()).toEqual([1, 3, 4, 5, 6, 7]);
+    a = a.remove(8);
+    expect([...a]).toEqual([1, 3, 4, 5, 6, 7]);
     // left child only
-    a.remove(7);
-    expect(a.toArray()).toEqual([1, 3, 4, 5, 6]);
+    a = a.remove(7);
+    expect([...a]).toEqual([1, 3, 4, 5, 6]);
     // both children
-    a.remove(3);
-    expect(a.toArray()).toEqual([1, 4, 5, 6]);
+    a = a.remove(3);
+    expect([...a]).toEqual([1, 4, 5, 6]);
     // need to remove some others to test right-only...
-    a.remove(1);
-    expect(a.toArray()).toEqual([4, 5, 6]);
-    a.remove(4);
-    expect(a.toArray()).toEqual([5, 6]);
+    a = a.remove(1);
+    expect([...a]).toEqual([4, 5, 6]);
+    a = a.remove(4);
+    expect([...a]).toEqual([5, 6]);
     // right only
-    a.remove(5);
-    expect(a.toArray()).toEqual([6]);
-    a.remove(6);
-    expect(a.toArray()).toEqual([]);
-    // check removal of non-existant element
-    a.remove(6);
-    expect(a.toArray()).toEqual([]);
+    a = a.remove(5);
+    expect([...a]).toEqual([6]);
+    a = a.remove(6);
+    expect(a).toBeUndefined();
     // needed to test a few fringe cases
     a = new BinarySearchTree([5, 3, 7, 1, 4, 6, 6.5, 8]);
-    a.remove(5);
-    expect(a.toArray()).toEqual([1, 3, 4, 6, 6.5, 7, 8]);
+    a = a.remove(5);
+    expect([...a]).toEqual([1, 3, 4, 6, 6.5, 7, 8]);
   });
 
   /** @test {BinarySearchTree#contains} */
@@ -79,8 +70,8 @@ describe('DS - BinarySearchTree', () => {
   /** @test {BinarySearchTree#getPredecessor} */
   it('should find predecessor values', () => {
     const a = new BinarySearchTree([5, 3, 7, 1, 4, 6, 8]);
-    expect(a.getPredecessor(1)).toBeNull();
-    expect(a.getPredecessor(2)).toBeNull();
+    expect(a.getPredecessor(1)).toBeUndefined();
+    expect(a.getPredecessor(2)).toBeUndefined();
     expect(a.getPredecessor(3)).toEqual(1);
     expect(a.getPredecessor(4)).toEqual(3);
     expect(a.getPredecessor(5)).toEqual(4);
@@ -92,8 +83,8 @@ describe('DS - BinarySearchTree', () => {
   /** @test {BinarySearchTree#getSuccessor} */
   it('should find successor values', () => {
     const a = new BinarySearchTree([5, 3, 7, 1, 4, 6, 8]);
-    expect(a.getSuccessor(8)).toBeNull();
-    expect(a.getSuccessor(2)).toBeNull();
+    expect(a.getSuccessor(8)).toBeUndefined();
+    expect(a.getSuccessor(2)).toBeUndefined();
     expect(a.getSuccessor(1)).toEqual(3);
     expect(a.getSuccessor(3)).toEqual(4);
     expect(a.getSuccessor(4)).toEqual(5);
@@ -105,6 +96,40 @@ describe('DS - BinarySearchTree', () => {
   /** @test {BinarySearchTree#constructor} */
   it('should take a comparison function to determine sort order', () => {
     const t = new BinarySearchTree([5, 3, 1, 7, 6, 9], (a, b) => a > b);
-    expect(t.toArray()).toEqual([9, 7, 6, 5, 3, 1]);
+    expect([...t]).toEqual([9, 7, 6, 5, 3, 1]);
+  });
+
+  /** @test {BinarySearchTree#@@iterable} */
+  describe('iterables', () => {
+    const t = new BinarySearchTree([5, 3, 1, 7, 6, 9]);
+
+    it('should default to inorder', () => {
+      expect([...t]).toEqual([1, 3, 5, 6, 7, 9]);
+    });
+
+    it('should have an explicit inorder iterable', () => {
+      expect([...t.inorder]).toEqual([1, 3, 5, 6, 7, 9]);
+    });
+
+    it('should have a reversed inorder iterable', () => {
+      expect([...t.inorderReversed]).toEqual([9, 7, 6, 5, 3, 1]);
+    });
+
+    it('should have a preorder iterable', () => {
+      expect([...t.preorder]).toEqual([5, 3, 1, 7, 6, 9]);
+    });
+
+    it('should have a right-to-left preorder iterable', () => {
+      expect([...t.preorderReversed]).toEqual([5, 7, 9, 6, 3, 1]);
+    });
+
+    it('should have a postorder iterable', () => {
+      debugger;
+      expect([...t.postorder]).toEqual([1, 3, 6, 9, 7, 5]);
+    });
+
+    it('should have a right-to-left postorder iterable', () => {
+      expect([...t.postorderReversed]).toEqual([9, 6, 7, 1, 3, 5]);
+    });
   });
 });
